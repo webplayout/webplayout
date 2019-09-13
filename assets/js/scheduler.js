fa02923329277323c1ac214ad9b65862b14bdf6a
Nikita Omarev <nikita@omarev.com>
2019-09-13 13:03:37 +0300

    remove redundant code

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -5,7 +5,3 @@ import { render } from 'react-dom'
 import Calendar from './components/Calendar'
 
 render(<Calendar />, document.getElementById('root'))
-
-// import Schedule from './components/Schedule/Schedule'
-//
-// render(<Schedule />, document.getElementById('root'))

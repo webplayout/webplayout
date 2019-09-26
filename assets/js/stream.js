cac5cfb425f81c4452f18eaf968db73d695dcf92
Nikita Omarev <nikita@omarev.com>
2019-09-26 21:47:43 +0300

    rm videojs-contrib-hls videojs-flash, redundant since video.js 7+

--- assets/js/stream.js
+++ assets/js/stream.js
@@ -2,5 +2,3 @@ require('../global.scss');
 require('video.js/dist/video-js.css')
 
 window.videojs = require('video.js/dist/video.js')
-require('videojs-flash/dist/videojs-flash.js')
-require('videojs-contrib-hls/dist/videojs-contrib-hls.js')
